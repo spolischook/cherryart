8186d93ef9e4578b28fcfe2a083953b5896f498e
Serhii Polishchuk <spolishchuk@oroinc.com>
2016-12-26 16:14:24 +0700

    Run tests on travis ci

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cherryart
+# cherryart  [![Build Status](https://travis-ci.org/spolischook/cherryart.svg?branch=master)](https://travis-ci.org/spolischook/cherryart)
 Personal site for Tetiana Cherevan
--- src/Cherry/Tests/WebTestCase.php
+++ src/Cherry/Tests/WebTestCase.php
@@ -2,7 +2,7 @@
 
 namespace Cherry\Tests;
 
-use Cherry\Application;
+use Cherry\FrontendApplication;
 use Silex\WebTestCase as BaseWebTestCase;
 
 class WebTestCase extends BaseWebTestCase
@@ -12,18 +12,10 @@ class WebTestCase extends BaseWebTestCase
      */
     public function createClient(array $server = [])
     {
-        if (isset($server['HTTP_HOST'])) {
-            return parent::createClient($server);
-        }
-
-        if (!isset($GLOBALS['http_host'])) {
-            return parent::createClient($server);
-        }
-
         return parent::createClient(
             array_merge(
                 $server,
-                ['HTTP_HOST' => $GLOBALS['http_host']]
+                ['HTTP_HOST' => WEB_SERVER_HOST.':'.WEB_SERVER_PORT]
             )
         );
     }
@@ -33,7 +25,7 @@ class WebTestCase extends BaseWebTestCase
      */
     public function createApplication()
     {
-        $app = new Application();
+        $app = new FrontendApplication();
         $app['debug'] = true;
 
         return $app;
